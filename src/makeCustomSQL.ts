dace45c91b93a6fc7a6119fb81cd1a761bb7ad7e
Omie Sawie <omkarsawant20HG@gmail.com>
2024-05-29 17:42:38 +0530

    Parameters extraction from URL and insertion in SQL

--- src/makeCustomSQL.ts
+++ src/makeCustomSQL.ts
@@ -1,16 +1,28 @@
 import mysql from "mysql2"
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: __dirname + '/.env' });
 
 const pool = mysql.createPool({
-  host: 'process.env.HOST',
-  user: 'process.env.',
-  password: 'root',
-  database: 'restSQL'
+  host: process.env.HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_USER_PASSWORD,
+  database: process.env.DATABASE,
 }).promise();
 
-const sendSQL = async (params: Object) => {
+console.log(process.env.DATABASE);
 
-  const result = await pool.query('SELECT * FROM tutorials WHERE id = 2 ');
-  // console.log(result[0]);
+// Define the type for params
+type Params = {
+  [key: string]: any; // or more specific types if you know them
+};
+
+const sendSQL = async (params: Params) => {
+  for (let key in params) {
+    console.log(key, params[key]);
+    const result = await pool.query(` SELECT * FROM tutorials WHERE title = ? `,  params[key]);
+    console.log(result);
+  }
 }
 
 export default sendSQL;
